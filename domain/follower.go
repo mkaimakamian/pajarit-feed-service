c0bfa12be86584a44a4eaddbfaa92b58a7991c8c
Kokumo <mkc@outlook.com.ar>
2025-04-01 17:56:15 -0300

    minnor comment changes

--- domain/follower.go
+++ domain/follower.go
@@ -1,8 +1,8 @@
 package domain
 
-// Representa una relación muy básica de seguidor - seguido
+// Representa una relación muy básica de seguidor - seguido.
 // Idealmente debería contar con un atributo que de idea de la fecha de alta;
-// otros datos de auditoría podríuan ser útiles también a menos que se audite por otro medio
+// otros datos de auditoría también podrían ser útiles a menos que se audite por otro medio.
 type Follower struct {
 	FollowerId string
 	FollowedId string
